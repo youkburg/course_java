8df237276999b6329abb3b8af13974156d3f1f2a
youkburg <youcef_kabli@hotmail.fr>
2021-04-12 12:07:58 +0100

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); // this line print  a Hello world message
     }
 }
--- src/firstClass.java
+++ src/firstClass.java
@@ -1,5 +1,5 @@
 public class firstClass {
-    int x=22;
-    System.out.println(x);
+    //this is just a test
+    
 
 }
